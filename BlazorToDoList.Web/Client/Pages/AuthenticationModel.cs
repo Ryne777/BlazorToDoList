d591b5e2db943caf051e878c44120d49a4528301
A. Ogudov <ryne777@yandex.ru>
2020-12-15 16:53:26 +0700

    Try add authecation - faild

---
+++ BlazorToDoList.Web/Client/Pages/AuthenticationModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorToDoList.Web.Client.Pages
+{
+    public class AuthenticationModel: ComponentBase
+    {
+        [Parameter] 
+        public string Action { get; set; }
+    }
+}
--- BlazorToDoList.Web/Client/Program.cs
+++ BlazorToDoList.Web/Client/Program.cs
@@ -1,8 +1,9 @@
 using MatBlazor;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Net.Http;
+using System;
 using System.Threading.Tasks;
 
 namespace BlazorToDoList.Web.Client
@@ -14,8 +15,14 @@ namespace BlazorToDoList.Web.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("BlazorToDoList.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorToDoList.ServerAPI"));
             builder.Services.AddMatBlazor();
+            builder.Services.AddOptions();
+            builder.Services.AddApiAuthorization();
 
             await builder.Build().RunAsync();
         }
